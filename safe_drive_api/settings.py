b3098d7197d40af2a196127ed9340dae2b21cb85
Leonidas MV <yordy.lmv.2000@gmail.com>
2025-03-27 18:31:15 +0100

    update files

--- safe_drive_api/settings.py
+++ safe_drive_api/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', 
     'rest_framework_simplejwt.token_blacklist',
-    'api', 
     "users",
+    "detections"
 ]
 
 MIDDLEWARE = [
