80fe3f70837e0be421c43b238601e4752c9dfe12
Leonidas MV <yordy.lmv.2000@gmail.com>
2025-04-15 16:45:20 +0200

    fix -.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Update virtual environment..."
-python3.11 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 
 echo "Installing virtualenv..."
-python3.11 -m pip install virtualenv
+python -m pip install virtualenv
 
 echo "Creating virtual environment..."
-python3.11 -m venv env
+python -m venv env
 
 echo "Activating virtual environment..."
 source env/bin/activate
