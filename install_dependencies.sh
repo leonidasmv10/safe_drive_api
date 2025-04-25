42b736a77bb09fb9e355db7db761661a1fa9edf1
JhonySY <jhonysiyu@gmail.com>
2025-04-25 20:16:16 +0000

    fixed dependencies and cert

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-echo "Update virtual environment..."
-python -m pip install --upgrade pip
-
-echo "Installing virtualenv..."
-python -m pip install virtualenv
-
 echo "Creating virtual environment..."
 python -m venv env
 
 echo "Activating virtual environment..."
 source env/bin/activate
 
+echo "Updating pip inside the virtual environment..."
+pip install --upgrade pip
+
 echo "Installing API requirements..."
 pip install -r requirements.txt
 
 echo "Installation completed."
 read -p "Press Enter to exit..."
+
--- safe_drive_api/settings.py
+++ safe_drive_api/settings.py
@@ -155,3 +155,7 @@ SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://safedriveapi.shop',
+]
