0e96812f4696542f6e35be78355507ead86e535a
leonidasmv10 <59670185+leonidasmv10@users.noreply.github.com>
2025-04-25 17:09:40 +0200

    fix

--- driving/models.py
+++ driving/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User
 
 class DrivingHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # avg_speed = models.DecimalField(max_digits=6, decimal_places=2)
-    # total_hard_brakes = models.IntegerField()
     total_horns = models.IntegerField()
     total_sirens = models.IntegerField()
     registration_date = models.DateTimeField(auto_now_add=True)
